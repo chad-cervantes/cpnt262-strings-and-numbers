49fad6687a92e0b08d2381acbab32bccd908f698
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-10-06 23:13:45 -0600

    added four strings for the graded activity 2: strings and numbers in client.js

--- client.js
+++ client.js
@@ -1,7 +1,22 @@
-const alertMessage = "This is a website"; 
-alert(alertMessage);
+// const alertMessage = "This is a website"; 
+// alert(alertMessage);
 
-const userInput = prompt("Please Enter Text");
+// const userInput = prompt("Please Enter Text");
 
-console.log(userInput);
-document.querySelector("#user-input").innerHTML = userInput;
+// console.log(userInput);
+// document.querySelector("#user-input").innerHTML = userInput;
+
+//STRINGS//
+const userName = "One Piece"
+
+//Show the number of characters in your string with String.length;
+console.log(userName.length);
+
+//Show the first index of a word that can be found in your string with String.indexOf() 
+console.log(userName.indexOf("n"));
+
+//Show the first character of your string;
+console.log(userName.charAt(0));
+
+//Show the last character of your string;
+console.log(userName.charAt(8));
