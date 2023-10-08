ceee2fab85f2ea760a1b6c27680d8335e880ee16
Chad Cervantes <chad-daniel.cervantes@outlook.com>
2023-10-07 23:40:41 -0600

    deleted commented out stuff not related to assignment

--- client.js
+++ client.js
@@ -1,11 +1,3 @@
-// const alertMessage = "This is a website"; 
-// alert(alertMessage);
-
-// const userInput = prompt("Please Enter Text");
-
-// console.log(userInput);
-// document.querySelector("#user-input").innerHTML = userInput;
-
 //STRINGS//
 const userName = "One Piece"
 
